b067102970d900e85c1f2b54de638c265ebd0509
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-10-02 17:31:58 +0300

    followed through lesson

--- server/models.py
+++ server/models.py
@@ -19,6 +19,12 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    #adds Relatinship mapping the employee to related reviews
+    reviews = db.relationship("Review", back_populates="employee", cascade='all, delete-orphan')
+
+    #adds Relatinship mapping the employee to related onboardings
+    onboarding = db.relationship("Onboarding", uselist=False, back_populates="employee")
+
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
 
@@ -30,6 +36,12 @@ class Onboarding(db.Model):
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    # Adds a foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+
+    # Establish a relationship mapping the onboarding to related employee
+    employee = db.relationship("Employee", back_populates="onboarding", cascade='all, delete-orphan')
+
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
@@ -40,6 +52,12 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+
+    # Adds a foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey("employees.id"))
+
+    # Establish a relationship mapping the review to related employee
+    employee = db.relationship("Employee", back_populates="reviews")
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
